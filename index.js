f0b82fec5b67f4cd10a4ccdace607b2617087ab6
Matthew K <matthew190290@gmail.com>
2024-02-21 22:08:38 -0500

    quitando index de app.use

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, "public", "index.html")));
+app.use(express.static(path.join(__dirname, "public")));
 
 let db;
 
