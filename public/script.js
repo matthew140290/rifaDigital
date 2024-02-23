25e0d71bc838b4e5d3a18af2d427ddf12b9e7776
Matthew K <matthew190290@gmail.com>
2024-02-22 20:36:46 -0500

    agregando campor phone, whatsapp y quitando localstorage

--- public/script.js
+++ public/script.js
@@ -2,33 +2,71 @@ document.addEventListener("DOMContentLoaded", async function () {
   try {
     await obtenerEstadoDesdeServidor();
     await obtenerHistorialDesdeServidor();
-
     crearBotones();
+
+    // Configurar recarga automática cada 5 segundos
+    setInterval(async function () {
+      await obtenerEstadoDesdeServidor();
+      await obtenerHistorialDesdeServidor();
+    }, 2000); // 5000 milisegundos = 5 segundos
 
     document
       .getElementById("formularioRifa")
-      .addEventListener("submit", function (event) {
+      .addEventListener("submit", async function (event) {
         event.preventDefault();
-        mostrarInformacion();
+        const nombreInput = document.getElementById("nombre");
+        const numerosInput = document.getElementById("numeros");
+        const phoneInput = document.getElementById("phone");
+        const nombre = nombreInput.value.trim();
+        const numeros = numerosInput.value.trim().padStart(2, "0");
+        const phone = phoneInput.value.trim();
+
+        try {
+          const botonSeleccionado = await obtenerBotonSeleccionado();
+
+          if (
+            nombre === "" ||
+            numeros === "" ||
+            botonSeleccionado === "" ||
+            phone === ""
+          ) {
+            mostrarError(
+              "Por favor, complete todos los campos y seleccione un botón."
+            );
+            return;
+          }
+
+          const participante = {
+            nombre,
+            numero: `${botonSeleccionado}${numeros}`,
+            phone,
+          };
+
+          await guardarInformacion(participante);
+        } catch (error) {
+          console.error("Error al obtener el botón seleccionado:", error);
+        }
       });
   } catch (error) {
     console.error("Error durante la inicialización:", error);
   }
 });
 
+async function obtenerBotonSeleccionado() {
+  try {
+    const botonesSeleccionados = await obtenerEstadoDesdeServidor();
+    return botonesSeleccionados[0] || ""; // Devolver el primer elemento (si existe)
+  } catch (error) {
+    console.error("Error al obtener el botón seleccionado:", error);
+    throw error;
+  }
+}
+
 async function obtenerEstadoDesdeServidor() {
   try {
     const response = await fetch("/obtenerEstado");
     const data = await response.json();
-    const botonesSeleccionados = data.botonesSeleccionados || [];
-
-    localStorage.setItem(
-      "botonesSeleccionados",
-      JSON.stringify(botonesSeleccionados)
-    );
-
-    deshabilitarBotones();
-    ocultarBotonSeleccionado();
+    return data.botonesSeleccionados || [];
   } catch (error) {
     console.error("Error al obtener el estado desde el servidor:", error);
     throw error;
@@ -51,31 +89,32 @@ async function obtenerHistorialDesdeServidor() {
     throw error;
   }
 }
+async function guardarInformacion(participante) {
+  try {
+    const response = await fetch("/guardarInformacion", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(participante),
+    });
 
-function deshabilitarBotones() {
-  const botonesSeleccionados =
-    JSON.parse(localStorage.getItem("botonesSeleccionados")) || [];
+    const message = await response.text();
+    console.log(message);
 
-  const botones = document.querySelectorAll("button");
-  botones.forEach((boton) => {
-    boton.disabled = true;
+    mostrarInformacionEnPantalla(participante);
 
-    if (!botonesSeleccionados.includes(boton.id)) {
-      boton.disabled = false;
-    }
-  });
-}
-
-function ocultarBotonSeleccionado() {
-  const botonesSeleccionados =
-    JSON.parse(localStorage.getItem("botonesSeleccionados")) || [];
-
-  botonesSeleccionados.forEach((valor) => {
-    const botonSeleccionadoElement = document.getElementById(valor);
-    if (botonSeleccionadoElement) {
-      botonSeleccionadoElement.style.display = "none";
-    }
-  });
+    // Limpiar los campos del formulario
+    document.getElementById("nombre").value = "";
+    document.getElementById("numeros").value = "";
+    document.getElementById("phone").value = "";
+    mostrarError(""); // Limpiar mensaje de error
+  } catch (error) {
+    console.error("Error al enviar la información:", error);
+    mostrarError(
+      "Error al enviar la información. Por favor, inténtalo de nuevo."
+    );
+  }
 }
 
 function crearBotones() {
@@ -88,94 +127,73 @@ function crearBotones() {
     button.id = numero;
     button.textContent = numero;
     button.onclick = function () {
-      deshabilitarBotones();
-      this.style.display = "none";
       capturarValores(numero);
     };
     botonesContainer.appendChild(button);
   }
-
-  ocultarBotonSeleccionado();
 }
 
 async function capturarValores(numero) {
-  let botonesSeleccionados =
-    JSON.parse(localStorage.getItem("botonesSeleccionados")) || [];
-
-  if (!botonesSeleccionados.includes(numero)) {
-    await actualizarEstadoServidor(botonesSeleccionados.concat(numero));
-    botonSeleccionado = numero;
-    localStorage.setItem("botonSeleccionado", botonSeleccionado);
-  }
-
-  localStorage.setItem(
-    "botonesSeleccionados",
-    JSON.stringify(botonesSeleccionados)
-  );
+  await actualizarEstadoServidor([numero]);
+  mostrarMensajeSeleccionado(numero);
 }
 
-async function actualizarEstadoServidor(botonesSeleccionados) {
+function actualizarEstadoServidor(botonesSeleccionados) {
   try {
-    const response = await fetch("/actualizarEstado", {
+    return fetch("/actualizarEstado", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ botonesSeleccionados }),
+    }).then(async (response) => {
+      const data = await response.json();
+      console.log("Estado actualizado en el servidor:", data);
     });
-    const data = await response.json();
-    console.log("Estado actualizado en el servidor:", data);
   } catch (error) {
     console.error("Error al actualizar el estado en el servidor:", error);
     throw error;
   }
 }
 
-function mostrarInformacion() {
+async function mostrarInformacion() {
   console.log("La función mostrarInformacion() se está ejecutando.");
 
   const nombreInput = document.getElementById("nombre");
   const numerosInput = document.getElementById("numeros");
+  const phoneInput = document.getElementById("phone");
 
   const nombre = nombreInput.value.trim();
   const numeros = numerosInput.value.trim().padStart(2, "0");
+  const phone = phoneInput.value.trim();
 
-  if (nombre === "" || numeros === "") {
+  if (nombre === "" || numeros === "" || phone === "") {
     mostrarError("Por favor, complete todos los campos.");
     return;
   }
 
-  if (botonSeleccionado === "") {
-    mostrarError("Por favor, seleccione un y solo un botón.");
-    return;
+  try {
+    const botonSeleccionado = await obtenerBotonSeleccionado();
+
+    if (botonSeleccionado === "") {
+      mostrarError("Por favor, seleccione un botón.");
+      return;
+    }
+
+    const participante = {
+      nombre,
+      numero: `${botonSeleccionado}${numeros}`,
+    };
+
+    await guardarInformacion(participante);
+
+    // Limpiar los campos del formulario
+    nombreInput.value = "";
+    numerosInput.value = "";
+    mostrarError(""); // Limpiar mensaje de error
+  } catch (error) {
+    console.error("Error al mostrar la información:", error);
   }
-
-  const participante = {
-    nombre,
-    numero: `${botonSeleccionado}${numeros}`,
-  };
-
-  participantes.push(participante);
-  localStorage.setItem("participantes", JSON.stringify(participantes));
-
-  mostrarInformacionEnPantalla(participante);
-
-  fetch("/guardarInformacion", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(participante),
-  })
-    .then((response) => response.text())
-    .then((message) => {
-      console.log(message);
-      mostrarInformacionEnPantalla(participante);
-      nombreInput.value = "";
-      numerosInput.value = "";
-      mostrarError(""); // Limpiar mensaje de error
-    })
-    .catch((error) => console.error("Error al enviar la información:", error));
 }
 
 function mostrarInformacionEnPantalla(participante) {
@@ -186,6 +204,10 @@ function mostrarInformacionEnPantalla(participante) {
   resultadoDiv.textContent = `Información Guardada: ${participante.nombre}: ${participante.numero}`;
   mostrarHistorial(participantes, historialDiv);
   errorMessageDiv.textContent = "";
+
+  setTimeout(() => {
+    resultadoDiv.textContent = "";
+  }, 3000);
 }
 
 function mostrarError(mensaje) {
@@ -193,13 +215,39 @@ function mostrarError(mensaje) {
   errorMessageDiv.textContent = mensaje;
 }
 
+// function mostrarHistorial(participantes, historialDiv) {
+//   historialDiv.innerHTML = "<h2>Historial de Participantes</h2>";
+//   if (participantes.length === 0) {
+//     historialDiv.innerHTML += "<p>No hay participantes aún.</p>";
+//   } else {
+//     participantes.forEach(function (participante) {
+//       historialDiv.innerHTML += `<p>${participante.nombre}: ${participante.numero}</p>`;
+//     });
+//   }
+// }
+
 function mostrarHistorial(participantes, historialDiv) {
-  historialDiv.innerHTML = "<h2>Historial de Participantes</h2>";
+  // Selecciona la tabla en lugar de div
+  const table = document.getElementById("tablaHistorial");
+  const tbody = table.querySelector("tbody");
+
+  // Limpia el contenido anterior del cuerpo de la tabla
+  tbody.innerHTML = "";
+
   if (participantes.length === 0) {
-    historialDiv.innerHTML += "<p>No hay participantes aún.</p>";
+    // Agrega una fila indicando que no hay participantes
+    const emptyRow = tbody.insertRow();
+    const cell = emptyRow.insertCell(0);
+    cell.colSpan = 2;
+    cell.textContent = "No hay participantes aún.";
   } else {
+    // Agrega una fila por cada participante
     participantes.forEach(function (participante) {
-      historialDiv.innerHTML += `<p>${participante.nombre}: ${participante.numero}</p>`;
+      const row = tbody.insertRow();
+      const cell1 = row.insertCell(0);
+      const cell2 = row.insertCell(1);
+      cell1.textContent = participante.nombre;
+      cell2.textContent = participante.numero;
     });
   }
 }
@@ -240,4 +288,15 @@ function copiarAlPortapapeles(texto) {
     // Eliminar el campo de texto temporal del DOM
     document.body.removeChild(inputTemp);
   }
+}
+
+function mostrarMensajeSeleccionado(numero) {
+  const mensajeSeleccionado = document.getElementById("mensaje-seleccionado");
+  mensajeSeleccionado.textContent = `Botón seleccionado: ${numero}`;
+  mensajeSeleccionado.style.display = "block";
+
+  // Ocultar el mensaje después de un tiempo (ej. 2 segundos)
+  setTimeout(() => {
+    mensajeSeleccionado.style.display = "none";
+  }, 3000);
 }
